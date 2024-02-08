2b9bf83cd50ff16995cba33e42f6b2a2254a6bb9
Alexander Medina <alexandermed219@gmail.com>
2024-02-07 20:19:42 -0500

    First launch

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div className='Ship-info-container'>
+        <h3 className='Ship-info'>FREE SHIPPING ON ALL ORDERS OVER $75+ (US DOMESTIC ONLY)</h3>
+      </div>
+      <h1 className='Title'>LIMITLESS ATHLETICS</h1>
+    </div>
+  );
+}
+
+export default App;
